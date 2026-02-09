1af847c615e254fe2f7f8842669191dc486f97e7
Georgi Angelov <gtangelov76@gmail.com>
2026-02-09 18:25:47 +0200

    Update kates_way_out.py

---
+++ kates_way_out.py
@@ -0,0 +1,12 @@
+number_of_rows = int(input())
+maze = []
+for row in range(number_of_rows):
+    current_row = input()
+    maze.append(current_row)
+
+    if "k" in current_row:
+        col = current_row.index("k")
+        k_row = row
+
+
+
