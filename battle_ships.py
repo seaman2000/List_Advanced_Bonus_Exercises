1fb659de68fe142a3caab0965254f2f8c20c67bf
Georgi Angelov <gtangelov76@gmail.com>
2026-02-09 19:15:02 +0200

    added tasks

--- battle_ships.py
+++ battle_ships.py
@@ -5,4 +5,13 @@ for row in range(rows):
     field.append(current_row)
 
 attacked = input().split()
-attacked_field = [attack.split("-") for attack in attacked]
+attacked_field = [[int(x) for x in attack.split("-")] for attack in attacked]
+
+destroyed = 0
+for row, col in attacked_field:
+    if field[row][col] > 0:
+        field[row][col] -= 1
+        if field[row][col] == 0:
+            destroyed += 1
+
+print(destroyed)
