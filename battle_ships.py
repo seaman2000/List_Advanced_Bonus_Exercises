c584170ea720848ec3d17129b38b5d3a66c322df
Georgi Angelov <gtangelov76@gmail.com>
2026-02-09 19:09:02 +0200

    Update battle_ships.py

--- battle_ships.py
+++ battle_ships.py
@@ -1,4 +1,8 @@
+field = []
 rows = int(input())
 for row in range(rows):
     current_row = list(map(int, input().split()))
-    
+    field.append(current_row)
+
+attacked = input().split()
+attacked_field = [attack.split("-") for attack in attacked]
